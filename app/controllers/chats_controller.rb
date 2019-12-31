a20472280cd18dc71aa5c335655df53141cd10fc
Stephen Keating <stephen.keating@gmail.com>
2019-12-31 14:02:07 -0500

    create chat added

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,20 @@
 class ChatsController < ApplicationController
+
+  def create
+    @chat = Chat.create(chat_params)
+    render json: @chat
+  end
+
+  def destroy
+    @chat = Chat.find(params[:id])
+    @chat.destroy
+    render json: {status: 'SUCCESS', message: 'deleted the chat', data: @chat}
+  end
+
+  private
+
+  def chat_params
+    params.permit(:message, :channel_id, :user_id)
+  end
+
 end
